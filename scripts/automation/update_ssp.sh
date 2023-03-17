d954ffad2e7ca1711ee698701efb2dc3cdd83e77
Ekaterina Nikonova <enikonovad@gmail.com>
2023-03-17 15:19:01 +1100

    fix: Update ssp

--- scripts/automation/update_ssp.sh
+++ scripts/automation/update_ssp.sh
@@ -4,7 +4,7 @@ export COMMIT_TITLE="chore: Profiles automatic update."
 export COMMIT_BODY="Sync profiles with ocp-oscal-profiles repo"
 git config --global user.email "automation@example.com"
 git config --global user.name "AutomationBot" 
-cd ocp-oscal-cd
+cd ocp-oscal-ssp
 git checkout -b "profiles_autoupdate_$GITHUB_RUN_ID"
 cp -r ../catalogs .
 cp -r ../profiles . 
